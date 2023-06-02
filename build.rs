6b01bdcdba3dd6d82179b1a4d2139eebae6569eb
Dimitri Bonanni-Surprenant <bond2102@usherbrooke.ca>
2023-06-02 18:59:22 +0000

    Resolved missing directory in out_dir.

--- build.rs
+++ build.rs
@@ -1,20 +1,29 @@
 // build script
 use std::process::Command;
-use std::fs::create_dir;
+use std::fs::create_dir_all;
 use std::path::Path;
 use std::env;
 
 fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_dir = Path::new(&out_dir);
+    let c_interface = out_dir.join(Path::new("c_interface"));
+    let fortran = out_dir.join(Path::new("fortran"));
+
+    // Create the c_interface directory
+    create_dir_all(c_interface.clone())
+        .expect("Can't create c_interface directory");
+    // Create the fortran directory
+    create_dir_all(fortran.clone())
+        .expect("Can't create fortran directory");
 
     // Compile source
     Command::new("make").output()
         .expect("Failed to make");
 
 
-    println!("cargo:rustc-link-search=c_interface");
-    println!("cargo:rustc-link-search=fortran");
-    println!("cargo:rustc-link-lib=static=pfapack");
-    println!("cargo:rustc-link-lib=static=cpfapack");
+    println!("cargo:rustc-link-search={}", c_interface.display());
+    println!("cargo:rustc-link-search={}", fortran.display());
     println!("cargo:rustc-link-lib=gfortran");
     println!("cargo:rustc-link-lib=lapack");
     println!("cargo:rustc-link-lib=blas");
